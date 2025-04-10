79aab178e4bc406b1b59d91c8051f74d487d5095
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-10 13:08:16 +0300

    Add files via upload

---
+++ shortcut-num.js
@@ -0,0 +1,25 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    for (let i = 1; i <= 6; i++) {
+        const span = document.getElementById(`count-shortcut${i}`);
+
+        if (span) {
+            // En yakın <a class="uk-accordion-title"> öğesini bul
+            const accordionTitle = span.closest("a.uk-accordion-title");
+
+            if (accordionTitle) {
+                const accordionContent = accordionTitle.nextElementSibling;
+
+                if (accordionContent) {
+                    // Accordion içeriğinde bir tablo varsa <tr> öğelerini say
+                    const table = accordionContent.querySelector("table");
+                    if (table) {
+                        const trCount = table.querySelectorAll("tr").length;
+                        span.textContent = `(${trCount} KISAYOL)`;
+                    }
+                }
+            }
+        }
+    }
+});
+</script>
